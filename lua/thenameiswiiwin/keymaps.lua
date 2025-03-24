44b566fcb6db5ff4fbfe8d58eb4389e027ee896f
thenameiswiiwin <huyn.nguyen95@gmail.com>
2025-03-24 15:34:05 -0400

    fix(keymaps): replace <C-u> with <C-b> for half-page scrolling

--- lua/thenameiswiiwin/keymaps.lua
+++ lua/thenameiswiiwin/keymaps.lua
@@ -10,7 +10,7 @@ vim.keymap.set("n", "J", "mzJ`z")
 
 -- Keep cursor centered when jumping
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-b>", "<C-u>zz")
 
 -- Keep search results centered
 vim.keymap.set("n", "n", "nzzzv")
